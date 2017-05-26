9f9bfa3a514822da16b26ee41169200ae4137d76
titangene <titangene01@gmail.com>
2017-05-26 11:21:47 +0800

    Changed car move logic 2

--- arduino-car.ino
+++ arduino-car.ino
@@ -1,27 +1,21 @@
 #include <Servo.h>         // Include Servo Motor library
 
 // L298N 的控制訊號腳位
-#define Left_Motor_IN1 = 13;
-#define Left_Motor_IN2 = 12;
-#define Right_Motor_IN3 = 11;
-#define Right_Motor_IN4 = 10;
+const byte Left_Motor_IN1 = 13, Left_Motor_IN2 = 12;
+const byte Right_Motor_IN3 = 11, Right_Motor_IN4 = 10;
 
 // SG90 超音波
 //Input pin to receive echo pulse
-#define Echo_Pin_L = 9;
-#define Echo_Pin_R = 7;
-#define Echo_Pin_C = 5;
+const byte Echo_Pin_L = 9, Echo_Pin_R = 7, Echo_Pin_C = 5;
 //Output pin to send trigger signal
-#define Trig_Pin_L = 8;
-#define Trig_Pin_R = 6;
-#define Trig_Pin_C = 4;
+const byte Trig_Pin_L = 8, Trig_Pin_R = 6, Trig_Pin_C = 4;
 unsigned long dist_L, dist_R, dist_C;
 
 // 伺服馬達 (SG90)
-#define ServoPin = 3;
+const byte ServoPin = 3;
 Servo servoMotor;  // create servo object to control a servo
 
-const byte MIN_Range = 5, MAX_Range = 7, OVER_Range = 10;
+const byte MIN_Range = 3, MAX_Range = 5, OVER_Range = 10;
 const short OUT_Range = 3400;
 const short rotation_180 = 1200, rotation_90 = 600, rotation_60 = 400, rotation_30 = 200, rotation_15 = 100;
 byte servo_Pos = 0;    // variable to store the servo position
@@ -61,35 +55,52 @@ void loop() {
 }
 
 void carMove() {
-    if (dist_C > MAX_Range) {
+    if (dist_C >= OUT_Range) {
+        coast(300);
+        backward(500);
+
+        if (dist_L > MIN_Range && dist_R > MIN_Range) {
+            rotateRandom();
+        } else if (dist_L <= MIN_Range && dist_R > MIN_Range) {
+            turnLeft(rotation_90);
+            coast(300);
+        } else if (dist_L > MIN_Range && dist_R <= MIN_Range) {
+            turnRight(rotation_90);
+            coast(300);
+        } else {
+            turnRight(rotation_180);
+            coast(300);
+        }
+
+    } else if (dist_C > MAX_Range && dist_C < OUT_Range) {
         if (dist_L > MIN_Range && dist_R > MIN_Range) {
             forward(500);
         } else if (dist_L <= MIN_Range && dist_R > MIN_Range) {
             turnLeft(rotation_15);
-            coast(500);
+            coast(300);
         } else if (dist_L > MIN_Range && dist_R <= MIN_Range) {
             turnRight(rotation_15);
-            coast(500);
+            coast(300);
         } else {
             turnRight(rotation_180);
-            coast(500);
+            coast(300);
         }
         
     } else {
-        coast(500);
+        coast(300);
         backward(300);
 
         if (dist_L > MIN_Range && dist_R > MIN_Range) {
             rotateRandom();
         } else if (dist_L <= MIN_Range && dist_R > MIN_Range) {
             turnLeft(rotation_90);
-            coast(500);
+            coast(300);
         } else if (dist_L > MIN_Range && dist_R <= MIN_Range) {
             turnRight(rotation_90);
-            coast(500);
+            coast(300);
         } else {
             turnRight(rotation_180);
-            coast(500);
+            coast(300);
         }
     }
 }
@@ -245,19 +256,20 @@ void setServoMotor(byte ServoPin) {
 
 void ServoMotor_180(byte EchoPin, byte TrigPin) {
     //Ultrasound(EchoPin, TrigPin);
-    for (pos = 0; servo_Pos <= 180; servo_Pos += 1) { // goes from 0 degrees to 180 degrees
+    for (servo_Pos = 0; servo_Pos <= 180; servo_Pos += 1) { // goes from 0 degrees to 180 degrees
         // in steps of 1 degree
-        servoMotor.write(pos);              // tell servo to go to position in variable 'pos'
+        servoMotor.write(servo_Pos);              // tell servo to go to position in variable 'pos'
         delay(15);                       // waits 15ms for the servo to reach the position
 
         //Ultrasound(EchoPin, TrigPin);
     }
-    for (pos = 180; servo_Pos >= 0; servo_Pos -= 1) { // goes from 180 degrees to 0 degrees
-        servoMotor.write(pos);              // tell servo to go to position in variable 'pos'
+    for (servo_Pos = 180; servo_Pos >= 0; servo_Pos -= 1) { // goes from 180 degrees to 0 degrees
+        servoMotor.write(servo_Pos);              // tell servo to go to position in variable 'pos'
         delay(15);                       // waits 15ms for the servo to reach the position
 
         //Ultrasound(EchoPin, TrigPin);
     }
 }
+
 
 
