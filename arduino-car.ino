64d3a28059f33c90d2f07a08db275b07ed754794
titangene <titangene01@gmail.com>
2017-06-08 16:19:16 +0800

    Change Ultrasound get distance logic

--- arduino-car.ino
+++ arduino-car.ino
@@ -1,244 +1,200 @@
-#include <Servo.h>         // Include Servo Motor library
-
-// L298N 的控制訊號腳位
-const byte Left_Motor_IN1 = 13, Left_Motor_IN2 = 12;
-const byte Right_Motor_IN3 = 11, Right_Motor_IN4 = 10;
-
 // SG90 超音波
 //Input pin to receive echo pulse
-const byte Echo_Pin_L = 9, Echo_Pin_R = 7, Echo_Pin_C = 5;
+const int Echo_Pin_L = 9, Echo_Pin_R = 7, Echo_Pin_C = 5;
 //Output pin to send trigger signal
-const byte Trig_Pin_L = 8, Trig_Pin_R = 6, Trig_Pin_C = 4;
-unsigned long dist_L, dist_R, dist_C;
+const int Trig_Pin_L = 8, Trig_Pin_R = 6, Trig_Pin_C = 4;
 
-// 伺服馬達 (SG90)
-const byte ServoPin = 3;
-Servo servoMotor;  // create servo object to control a servo
+unsigned int dist_C[15], dist_L[15], dist_R[15];
+unsigned int tmpDist_C, tmpDist_L, tmpDist_R, dist_L_AVE;
+unsigned int dist_C_Throw[15], dist_L_Throw[15], dist_R_Throw[15];
 
-const byte MIN_Range = 3, MAX_Range = 5, OVER_Range = 10;
-const short OUT_Range = 3400;
-const short rotation_180 = 1200, rotation_90 = 600, rotation_60 = 400, rotation_30 = 200, rotation_15 = 100;
-byte servo_Pos = 0;    // variable to store the servo position
-String logStr = "";
+const int perSecGetDist = 10;
+int i, count = 0;
+
+// 車寬 13.5 cm，左右邊超音波可能最長距離 86.5 cm
+const float dist_LR_max = 86.5;
+// 即時檢查目前左右邊超音波可能最長距離 (EX：L = 20, 那 R 一定 <= 66.5 )
+float dist_L_current_max = 86.5, dist_R_current_max = 86.5;
+
+// 每格地 100 cm，假設場地最大 10 x 10 片，正面超音波可能最長距離 1000 cm
+const float dist_C_max = 1000;
+
+unsigned long previous_time;
+unsigned long current_time;
+unsigned long calc_time;
+
+bool L_Flag = false;
+bool R_Flag = false;
+#define true 1
+#define false 0
+
+int x = 0;
+int i_C = 0, i_L = 0, i_R = 0;
+unsigned int dis_C[20], dis_L[20], dis_R[20];
 
 void setup() {
-    // 設定L298n控制腳位為輸出
-    setCarTire();
     // 超音波 (HC-SR04)
     setUltrasound(Trig_Pin_L);
     setUltrasound(Trig_Pin_R);
     setUltrasound(Trig_Pin_C);
-    // 伺服馬達 (SG90)
-    //setServoMotor(ServoPin);
 }
 
 void loop() {
-    dist_L = Ultrasound(Echo_Pin_L, Trig_Pin_L);
-    dist_R = Ultrasound(Echo_Pin_R, Trig_Pin_R);
-    dist_C = Ultrasound(Echo_Pin_C, Trig_Pin_C);
-
-    Serial.print(logStr); Serial.print(" - ");
-    Serial.print(dist_L); Serial.print(", ");
-    Serial.print(dist_C); Serial.print(", ");
-    Serial.println(dist_R);
-
-    carMove();    
-    delay(50);
-
-    // forward(1500);
-    // backward(1500);
-    // turnLeft(600);
-    // turnRight(600);
-    // coast(300);
-
-    //ServoMotor_180(Echo_Pin_C, Trig_Pin_C);
+    get_dist();
+    delay(1000);
 }
 
-void carMove() {
-    if (dist_C >= OUT_Range) {
-        coast(300);
-        backward(500);
+void get_dist() {
+    i_C = 0, i_L = 0, i_R = 0, calc_time = 0;
+    get_dist_CLR(0, 0);
+    get_dist_CLR(0, 0);
+    get_dist_CLR(0, 0);
+    get_dist_CLR(0, 0);
+    get_dist_CLR(0, 0);
 
-        if (dist_L > MIN_Range && dist_R > MIN_Range) {
-            rotateRandom();
-        } else if (dist_L <= MIN_Range && dist_R > MIN_Range) {
-            turnLeft(rotation_90);
-            coast(300);
-        } else if (dist_L > MIN_Range && dist_R <= MIN_Range) {
-            turnRight(rotation_90);
-            coast(300);
+    Serial.println(calc_time);
+    print_dist();
+    action(0);
+}
+
+void get_dist_CLR(bool L_Flag, bool R_Flag) {
+    get_dist_C();
+    get_dist_L();
+    if (L_Flag) {
+        get_dist_L();
+    }
+    if (R_Flag) {
+        get_dist_R();
+    }
+    get_dist_R();
+}
+void get_dist_C() {
+    previous_time = millis();
+    dist_C[i_C] = Ultrasound('C');
+    delay(60);
+    current_time = millis() - previous_time;
+    calc_time += current_time;
+    i_C++;
+}
+void get_dist_L() {
+    previous_time = millis();
+    dist_L[i_L] = Ultrasound('L');
+    delay(60);
+    current_time = millis() - previous_time;
+    calc_time += current_time;
+    i_L++;
+}
+void get_dist_R() {
+    previous_time = millis();
+    dist_R[i_R] = Ultrasound('R');
+    delay(60);
+    current_time = millis() - previous_time;
+    calc_time += current_time;
+    i_R++;
+}
+void print_dist() {
+    for (i = 0; i < i_C; i++) {
+        if (dist_C[i] < dist_LR_max) {
+            dist_L[i] = tmpDist_L;
+            dist_L_AVE += tmpDist_L;
         } else {
-            turnRight(rotation_180);
-            coast(300);
+            dist_L_Throw[count] = tmpDist_L;
+            count++;
         }
+        Serial.print(dist_C[i]);
+        Serial.print(", ");
+    }
+    Serial.println("");
+    for (i = 0; i < i_L; i++) {
+        Serial.print(dist_L[i]);
+        Serial.print(", ");
+    }
+    Serial.println("");
+    for (i = 0; i < i_R; i++) {
+        Serial.print(dist_R[i]);
+        Serial.print(", ");
+    }
+    Serial.println("");
+}
 
-    } else if (dist_C > MAX_Range && dist_C < OUT_Range) {
-        if (dist_L > MIN_Range && dist_R > MIN_Range) {
-            forward(500);
-        } else if (dist_L <= MIN_Range && dist_R > MIN_Range) {
-            turnLeft(rotation_15);
-            coast(300);
-        } else if (dist_L > MIN_Range && dist_R <= MIN_Range) {
-            turnRight(rotation_15);
-            coast(300);
-        } else {
-            turnRight(rotation_180);
-            coast(300);
-        }
-        
-    } else {
-        coast(300);
-        backward(300);
+void action(int dist) {
 
-        if (dist_L > MIN_Range && dist_R > MIN_Range) {
-            rotateRandom();
-        } else if (dist_L <= MIN_Range && dist_R > MIN_Range) {
-            turnLeft(rotation_90);
-            coast(300);
-        } else if (dist_L > MIN_Range && dist_R <= MIN_Range) {
-            turnRight(rotation_90);
-            coast(300);
+}
+
+void func() {
+    dist_L_AVE = 0, count = 0;
+    Serial.print(x);
+    Serial.print(" - ");
+    
+    for (i = 0; i < perSecGetDist; i++) {
+        tmpDist_L = Ultrasound('L');
+        Serial.print(tmpDist_L);
+        Serial.print(", ");
+
+        if (tmpDist_L < dist_LR_max) {
+            dist_L[i] = tmpDist_L;
+            dist_L_AVE += tmpDist_L;
         } else {
-            turnRight(rotation_180);
-            coast(300);
+            dist_L_Throw[count] = tmpDist_L;
+            count++;
         }
     }
-}
 
-void chooseRotation() {
-    coast(500);
-    backward(500);
-
-    if (dist_R < MIN_Range && dist_L > MAX_Range) {
-        turnLeft(rotation_90);
-        coast(500);
-    } else if (dist_L < MIN_Range && dist_R > MAX_Range) {
-        turnRight(rotation_90);
-        coast(500);
-    } else {
-        rotateRandom();
+    Serial.println("");
+    Serial.print("out: "); 
+    if (count != 0) {
+        for (i = 0; i < count; i++) {
+            Serial.print(dist_L_Throw[i]);
+            Serial.print(", ");
+        }
     }
+    count = 10 - count; // 將原本爆掉數值數量 改成 可用數值數量
+    // 如果有 3/10 以上數值都爆掉，大部分都是超音波離障礙物太近的原因，因此該方向超音波距離就設為 1 cm
+    dist_L_AVE = count >= 7 ? dist_L_AVE / count : 1;
+    
+    Serial.println("");
+    current_time = millis();
+    Serial.print(current_time);
+    Serial.print(" - ");
+    Serial.print(count);
+    Serial.print(" - ");
+    Serial.println(dist_L_AVE);
+    Serial.println("--------------------------------------");
+
+    delay(500);
 }
 
-void rotateRandom() {
-    // random number 0 ~ 1
-    long randomNumber = random(2);
-    if (randomNumber == 0)
-        turnLeft(rotation_90);
-    else
-        turnRight(rotation_90);
-}
-
-// 設定L298n控制腳位為輸出
-void setCarTire() {
-    pinMode(Left_Motor_IN1, OUTPUT);
-    pinMode(Left_Motor_IN2, OUTPUT);
-    pinMode(Right_Motor_IN3, OUTPUT);
-    pinMode(Right_Motor_IN4, OUTPUT);
-}
-// ------------------------------------------------
-void forward(int time) {
-    motorAForward();
-    motorBForward();
-    delay(time);
-    logStr = "F";
-}
- 
-void backward(int time) {
-    motorABackward();
-    motorBBackward();
-    delay(time);
-    logStr = "B";
-}
- 
-void turnLeft(int time) {
-    motorABackward();
-    motorBForward();
-    delay(time);
-    logStr = "L";
-}
- 
-void turnRight(int time) {
-    motorAForward();
-    motorBBackward();
-    delay(time);
-    logStr = "R";
-}
- 
-void coast(int time) {
-    motorACoast();
-    motorBCoast();
-    delay(time);
-    logStr = "C";
-}
- 
-void brake(int time) {
-    motorABrake();
-    motorBBrake();
-    delay(time);
-    logStr = "K";
-}
-// ------------------------------------------------
-//motor A controls
-void motorAForward() {
-    digitalWrite(Left_Motor_IN1, HIGH);
-    digitalWrite(Left_Motor_IN2, LOW);
-}
- 
-void motorABackward() {
-    digitalWrite(Left_Motor_IN1, LOW);
-    digitalWrite(Left_Motor_IN2, HIGH);
-}
- 
-//motor B controls
-void motorBForward() {
-    digitalWrite(Right_Motor_IN3, HIGH);
-    digitalWrite(Right_Motor_IN4, LOW);
-}
- 
-void motorBBackward() {
-    digitalWrite(Right_Motor_IN3, LOW);
-    digitalWrite(Right_Motor_IN4, HIGH);
-}
- 
-//coasting and braking
-void motorACoast() {
-    digitalWrite(Left_Motor_IN1, LOW);
-    digitalWrite(Left_Motor_IN2, LOW);
-}
- 
-void motorABrake() {
-    digitalWrite(Left_Motor_IN1, HIGH);
-    digitalWrite(Left_Motor_IN2, HIGH);
-}
- 
-void motorBCoast() {
-    digitalWrite(Right_Motor_IN3, LOW);
-    digitalWrite(Right_Motor_IN4, LOW);
-}
- 
-void motorBBrake() {
-    digitalWrite(Right_Motor_IN3, HIGH);
-    digitalWrite(Right_Motor_IN4, HIGH);
-}
 // ------------------------------------------------
 // 超音波 (HC-SR04)
-void setUltrasound(byte TrigPin) {
+void setUltrasound(int TrigPin) {
     pinMode(TrigPin, OUTPUT);   //set TrigPin as OUTPUT
     Serial.begin(9600);         //uart baud set 9600
 }
 
-long Ultrasound(byte EchoPin, byte TrigPin) {
-    // 音波速度每秒 343.2 公尺, 1/343.2 = 2.91 ms, 1公尺 2.91 ms, 每公分 29.1 us
+long Ultrasound(char c) {
+    // 音波速度 343.2 m/s, 1/343.2 = 2.91 m/s, 29.1 cm/us
     // 所以 echo, (high 的時間 uS / 2) / 29.1 => 
     // high 的時間 uS/(2 * 29.1) => high 的時間 uS / 58.2
-    unsigned long dist = ping(EchoPin, TrigPin) / 58;  //calculate distance
+    int EchoPin, TrigPin;
+    if (c == 'C') {
+        EchoPin = Echo_Pin_C;
+        TrigPin = Trig_Pin_C;
+    }
+    if (c == 'L') {
+        EchoPin = Echo_Pin_L;
+        TrigPin = Trig_Pin_L;
+    }
+    if (c == 'R') {
+        EchoPin = Echo_Pin_R;
+        TrigPin = Trig_Pin_R;
+    }
+    unsigned int dist = ping(EchoPin, TrigPin) / 58;  //calculate distance
     return dist;
 }
 
 // 超音波 high 的時間
-unsigned long ping(byte EchoPin, byte TrigPin) { 
-    //send 10us pulse to HC-SR04 trigger pin
+unsigned long ping(int EchoPin, int TrigPin) { 
+    // send 10us pulse to HC-SR04 trigger pin
     digitalWrite(TrigPin, HIGH);
     // 超音波只要超過 10us 就好
     delayMicroseconds(10);      //at least 10us HIGH pulse
@@ -246,30 +202,11 @@ unsigned long ping(byte EchoPin, byte TrigPin) {
     // 傳回 per pulse 時間
     return pulseIn(EchoPin, HIGH);
 }
-// ------------------------------------------------
-// 伺服馬達 (SG90)
-void setServoMotor(byte ServoPin) {
-    servoMotor.attach(ServoPin, 500, 2400); // 修正脈衝寬度範圍
-    servoMotor.write(90); //置中
-    delay(10000); 
-}
 
-void ServoMotor_180(byte EchoPin, byte TrigPin) {
-    //Ultrasound(EchoPin, TrigPin);
-    for (servo_Pos = 0; servo_Pos <= 180; servo_Pos += 1) { // goes from 0 degrees to 180 degrees
-        // in steps of 1 degree
-        servoMotor.write(servo_Pos);              // tell servo to go to position in variable 'pos'
-        delay(15);                       // waits 15ms for the servo to reach the position
 
-        //Ultrasound(EchoPin, TrigPin);
-    }
-    for (servo_Pos = 180; servo_Pos >= 0; servo_Pos -= 1) { // goes from 180 degrees to 0 degrees
-        servoMotor.write(servo_Pos);              // tell servo to go to position in variable 'pos'
-        delay(15);                       // waits 15ms for the servo to reach the position
 
-        //Ultrasound(EchoPin, TrigPin);
-    }
-}
+
+
 
 
 
