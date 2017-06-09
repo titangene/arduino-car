fec76e3039697aa6960fe0e736e7ca0e774ee1c3
titangene <titangene01@gmail.com>
2017-06-09 15:38:19 +0800

    Change Ultrasound get distance logic 3

--- arduino-car.ino
+++ arduino-car.ino
@@ -1,11 +1,19 @@
+// L298N 的控制訊號腳位
+const byte Left_Motor_IN1 = 13, Left_Motor_IN2 = 12;
+const byte Right_Motor_IN3 = 11, Right_Motor_IN4 = 10;
+
+const short rotation_180 = 1200, rotation_90 = 600, rotation_60 = 400, rotation_30 = 200, rotation_20 = 132, rotation_15 = 100;
+
 // SG90 超音波
 //Input pin to receive echo pulse
 const int Echo_Pin_L = 9, Echo_Pin_R = 7, Echo_Pin_C = 5;
 //Output pin to send trigger signal
 const int Trig_Pin_L = 8, Trig_Pin_R = 6, Trig_Pin_C = 4;
 
-unsigned int dist_C[15], dist_L[15], dist_R[15];
-unsigned int dist_L_AVE;
+
+const int dist_arr_length = 7;
+unsigned int dist_C[dist_arr_length], dist_L[dist_arr_length], dist_R[dist_arr_length];
+unsigned int dist_C_AVE = 0, dist_L_AVE = 0, dist_R_AVE = 0;
 unsigned int dist_C_Throw[15], dist_L_Throw[15], dist_R_Throw[15];
 int cut_C = 0, cut_L = 0, cut_R = 0;
 const int interval_length_OneDim = 7, 
@@ -23,6 +31,7 @@ const int get_dist_delay = 60;
 const float dist_LR_max = 16.5;
 // 即時檢查目前左右邊超音波可能最長距離 (EX：L = 10, 那 R 一定 <= 6.5 )
 const float dist_L_current_max = 16.5, dist_R_current_max = 16.5;
+const int dist_CLR_min = 4;
 // 每格邊長 30 cm，假設場地最大 30 x 5 片，正面超音波可能最長距離 150 cm
 const float dist_C_max = 150;
 
@@ -38,10 +47,16 @@ bool L_Flag = false, R_Flag = false;
 #define false 0
 
 int x = 0;
-int i_C = 0, i_L = 0, i_R = 0;
+int count_C = 0, count_L = 0, count_R = 0;
+int count_C_Return = 0, count_L_Return = 0;
 unsigned int dis_C[20], dis_L[20], dis_R[20];
 
+int j, AVE = 0;
+
+
 void setup() {
+    // 設定L298n控制腳位為輸出
+    setCarTire();
     // 超音波 (HC-SR04)
     setUltrasound(Trig_Pin_L);
     setUltrasound(Trig_Pin_R);
@@ -50,84 +65,168 @@ void setup() {
 
 void loop() {
     get_dist();
-    action(0);
-    delay(500);
+    Serial.println("-------------");
+    //delay(500);
 }
 
 void get_dist() {
-    i_C = 0, i_L = 0, i_R = 0;
-    cut_C = 0, cut_L = 0, cut_R = 0;
+    dist_C_AVE = 0, dist_L_AVE = 0, dist_R_AVE = 0, AVE = 0;
+    count_C = 0, count_L = 0, count_R = 0;
     previous_time = millis();
 
-    get_dist_CLR(0, 0);
-    get_dist_CLR(0, 0);
-    get_dist_CLR(0, 0);
+    action();
+    print_dist();
 
     current_time = millis() - previous_time;
+    Serial.print("t: ");
     Serial.println(current_time);
-
-    print_dist();
 }
-
-void get_dist_CLR(bool L_Flag, bool R_Flag) {
-    get_dist_C();
-    get_dist_L();
-    if (L_Flag) get_dist_L();
-    if (R_Flag) get_dist_R();
+void action() {
     get_dist_R();
-}
-void get_dist_C() {
-    dist_C[i_C] = Ultrasound('C');
-    delay(get_dist_delay);
-    i_C++;
-}
-void get_dist_L() {
-    dist_L[i_L] = Ultrasound('L');
-    delay(get_dist_delay);
-    i_L++;
+    // 右: 空 (dist >= 4) -> turnRight 15
+    if (dist_R_AVE >= dist_CLR_min) {
+        turnRight(rotation_30);
+        coast(500);
+        forward(800);
+    } else {
+        get_dist_C();
+        if (dist_C_AVE >= dist_CLR_min)
+            forward(800);
+        else {
+            get_dist_L();
+            if (dist_L_AVE >= dist_CLR_min) {
+                turnLeft(rotation_90);
+                coast(500);
+                //forward(1000);
+            } else {
+                turnLeft(rotation_180);
+                coast(500);
+                //forward(1000);
+            }
+        }
+    }
 }
 void get_dist_R() {
-    dist_R[i_R] = Ultrasound('R');
-    delay(get_dist_delay);
-    i_R++;
+    // 右: 有 (dist < 4) -> turnLeft 15
+    for (i = 0; i < dist_arr_length; i++) {
+        dist_R[i] = Ultrasound('R');
+        // 如果右邊超音波離牆壁距離低於 4 cm 就向左轉校正，
+        if (dist_R[i] < dist_CLR_min) {
+            turnLeft(rotation_20);
+            coast(500);
+        }
+        // 濾波
+        if(i <= 4) {
+            if (dist_R[i] < dist_C_max) {
+                dist_R_AVE += dist_R[i];
+                count_R++;
+            }
+            if (i == 4) {
+                AVE = dist_R_AVE;
+                dist_R_AVE = dist_R_AVE / count_R;
+                //Serial.println(AVE);
+            }
+        } else {
+            // 第二次 0~5, 第三次 0~6...
+            if (!(dist_R[i] > dist_R_AVE + 3 || dist_R[i] < dist_R_AVE - 3)) {
+                count_R++;
+                AVE += dist_R[i];
+                dist_R_AVE = AVE / count_R;
+            }
+        }
+
+        delay(get_dist_delay);
+    }
+    // 如果全部都是爆掉就設為場地最長值 (此方法不好)
+    if (count_R == 0) dist_R_AVE = dist_C_max;
 }
+void get_dist_C() {
+    // 中 or 左: 有 (dist < 4) -> turnLeft 15
+    for (i = 0; i < dist_arr_length; i++) {
+        dist_C[i] = Ultrasound('C');
+        // 濾波
+        if(i <= 4) {
+            if (dist_C[i] < dist_C_max) {
+                dist_C_AVE += dist_C[i];
+                count_C++;
+            }
+            if (i == 4) {
+                AVE = dist_C_AVE;
+                dist_C_AVE = dist_C_AVE / count_C;
+                //Serial.println(AVE);
+            }
+        } else {
+            // 第二次 0~5, 第三次 0~6...
+            if (!(dist_C[i] > dist_C_AVE + 3 || dist_C[i] < dist_C_AVE - 3)) {
+                count_C++;
+                AVE += dist_C[i];
+                dist_C_AVE = AVE / count_C;
+            }
+        }
+        delay(get_dist_delay);
+    }
+    // 如果全部都是爆掉就設為場地最長值 (此方法不好)
+    if (count_C == 0) dist_C_AVE = dist_C_max;
+}
+void get_dist_L() {
+    // 中 or 左: 有 (dist < 4) -> turnLeft 15
+    for (i = 0; i < dist_arr_length; i++) {
+        dist_L[i] = Ultrasound('L');
+        // 濾波
+        if(i <= 4) {
+            if (dist_L[i] < dist_C_max) {
+                dist_L_AVE += dist_L[i];
+                count_L++;
+            }
+            if (i == 4) {
+                AVE = dist_L_AVE;
+                dist_L_AVE = dist_L_AVE / count_L;
+                //Serial.println(AVE);
+            }
+        } else {
+            // 第二次 0~5, 第三次 0~6...
+            if (!(dist_L[i] > dist_L_AVE + 3 || dist_L[i] < dist_L_AVE - 3)) {
+                count_L++;
+                AVE += dist_L[i];
+                dist_L_AVE = AVE / count_L;
+            }
+        }
+        delay(get_dist_delay);
+    }
+    // 如果全部都是爆掉就設為場地最長值 (此方法不好)
+    if (count_L == 0) dist_L_AVE = dist_C_max;
+}
+// 濾波
+unsigned int filter(unsigned int distArr[], unsigned int dist_AVE, int count, int i) {
+    // // 第一次平均 0~4
+    // if(i <= 4) {
+    //     if (distArr[i] < dist_C_max) {
+    //         dist_AVE += distArr[i];
+    //         count++;
+    //     }
+    //     if (i == 4) {
+    //         AVE = dist_AVE;
+    //         dist_AVE = dist_AVE / count;
+    //         //Serial.println(AVE);
+    //     }
+    // } else {
+    //     // 第二次 0~5, 第三次 0~6...
+    //     if (!(distArr[i] > dist_AVE + 3 || distArr[i] < dist_AVE - 3)) {
+    //         count++;
+    //         AVE += distArr[i];
+    //         dist_AVE = AVE / count;
+    //     }
+    // }
+    // Serial.println(dist_AVE);
+    // return dist_AVE;
+}
+
 void print_dist() {
-    for (i = 0; i < i_C; i++) {
-        // if (dist_C[i] < dist_C_max) {
-        //     interval(interval_L, dist_C[i]);
-        // } else {
-        //     dist_C_Throw[count] = dist_C[i];
-        //     cut_C++;
-        // }
-        Serial.print(dist_C[i]);
-        Serial.print(", ");
-    }
-    Serial.println("");
-    for (i = 0; i < i_L; i++) {
-        // if (dist_L[i] < dist_L_current_max) {
-        //     interval(interval_L, dist_L[i]);
-        // }
-        Serial.print(dist_L[i]);
-        Serial.print(", ");
-        
-    }
-    Serial.println("");
-    for (i = 0; i < i_R; i++) {
-        Serial.print(dist_R[i]);
-        Serial.print(", ");
-    }
-    Serial.println("");
-}
-
-// void interval(unsigned int interval[][], unsigned int dist) {
-//     unsigned int dist_arr_i = dist >= 30 ? interval_length_OneDim : dist / 5;
-//     interval[]
-//     Serial.print(dist_arr_i);
-//     Serial.print(": ");
-// }
-
-void action(int dist) {
-
+    Serial.print(dist_C_AVE);
+    Serial.print(", ");
+    Serial.print(dist_L_AVE);
+    Serial.print(", ");
+    Serial.println(dist_R_AVE);
 }
 
 void func() {
@@ -173,6 +272,98 @@ void func() {
     delay(500);
 }
 
+// 設定L298n控制腳位為輸出
+void setCarTire() {
+    pinMode(Left_Motor_IN1, OUTPUT);
+    pinMode(Left_Motor_IN2, OUTPUT);
+    pinMode(Right_Motor_IN3, OUTPUT);
+    pinMode(Right_Motor_IN4, OUTPUT);
+}
+// ------------------------------------------------
+void forward(int time) {
+    motorAForward();
+    motorBForward();
+    delay(time);
+    Serial.println("Forward");
+}
+ 
+void backward(int time) {
+    motorABackward();
+    motorBBackward();
+    delay(time);
+    Serial.println("Backward");
+}
+ 
+void turnRight(int time) {
+    motorABackward();
+    motorBForward();
+    delay(time);
+    Serial.println("Right");
+}
+ 
+void turnLeft(int time) {
+    motorAForward();
+    motorBBackward();
+    delay(time);
+    Serial.println("Left");
+}
+ 
+void coast(int time) {
+    motorACoast();
+    motorBCoast();
+    delay(time);
+    Serial.println("Stop");
+}
+ 
+void brake(int time) {
+    motorABrake();
+    motorBBrake();
+    delay(time);
+    Serial.println("brake");
+}
+// ------------------------------------------------
+//motor A controls
+void motorAForward() {
+    digitalWrite(Left_Motor_IN1, HIGH);
+    digitalWrite(Left_Motor_IN2, LOW);
+}
+ 
+void motorABackward() {
+    digitalWrite(Left_Motor_IN1, LOW);
+    digitalWrite(Left_Motor_IN2, HIGH);
+}
+ 
+//motor B controls
+void motorBForward() {
+    digitalWrite(Right_Motor_IN3, HIGH);
+    digitalWrite(Right_Motor_IN4, LOW);
+}
+ 
+void motorBBackward() {
+    digitalWrite(Right_Motor_IN3, LOW);
+    digitalWrite(Right_Motor_IN4, HIGH);
+}
+ 
+//coasting and braking
+void motorACoast() {
+    digitalWrite(Left_Motor_IN1, LOW);
+    digitalWrite(Left_Motor_IN2, LOW);
+}
+ 
+void motorABrake() {
+    digitalWrite(Left_Motor_IN1, HIGH);
+    digitalWrite(Left_Motor_IN2, HIGH);
+}
+ 
+void motorBCoast() {
+    digitalWrite(Right_Motor_IN3, LOW);
+    digitalWrite(Right_Motor_IN4, LOW);
+}
+ 
+void motorBBrake() {
+    digitalWrite(Right_Motor_IN3, HIGH);
+    digitalWrite(Right_Motor_IN4, HIGH);
+}
 // ------------------------------------------------
 // 超音波 (HC-SR04)
 void setUltrasound(int TrigPin) {
@@ -211,6 +402,10 @@ unsigned long ping(int EchoPin, int TrigPin) {
     // 傳回 per pulse 時間
     return pulseIn(EchoPin, HIGH);
 }
+
+
+
+
 
 
 
