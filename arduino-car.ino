c3ca232b37697d11259c8749d208d5c29e809b91
titangene <titangene01@gmail.com>
2017-06-16 15:15:50 +0800

    Add include string, print function

--- arduino-car.ino
+++ arduino-car.ino
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <SoftwareSerial.h>
 
 // HC-06 藍牙模組
@@ -31,19 +32,19 @@ const float dist_L_current_max = 16.5, dist_R_current_max = 16.5;
 const int dist_CLR_min = 5;
 // 每格邊長 30 cm，假設場地最大 30 x 5 片，正面超音波可能最長距離 150 cm
 const float dist_C_max = 150;
-
+// fot 迴圈變數, 平均數, 執行次數
 int i, count = 0, AVE = 0, x = 1;
 
 unsigned long previous_time;
 unsigned long current_time;
 
 // 定義連接藍牙模組的序列埠
-SoftwareSerial BT(BL_TX_Pin, BL_RX_Pin); // 傳送腳, 接收腳 (TX / RX)
+SoftwareSerial BT(BL_TX_Pin, BL_RX_Pin); // 傳送腳 / 接收腳 (TX / RX)
 
 void setup() {
     Serial.begin(9600);   // 與電腦序列埠連線
     BT.begin(38400);   // 設定藍牙模組的連線速率
-    // 設定L298n控制腳位為輸出
+    // 設定 L298n 控制腳位為輸出
     setCarTire();
     // 超音波 (HC-SR04)
     setUltrasound(Trig_Pin_L);
@@ -55,32 +56,26 @@ void loop() {
     dist_C_AVE = 0, dist_L_AVE = 0, dist_R_AVE = 0;
     previous_time = millis();
 
+    dist_L_AVE = get_dist('L', dist_L, dist_L_AVE);
     dist_R_AVE = get_dist('R', dist_R, dist_R_AVE);
-    BT.print("R: ");
-    BT.println(dist_R_AVE);
+    print(x + String(" - L: ") + dist_L_AVE + ", R: " + dist_R_AVE);
+    
 
-    action();
-    //print_dist();
+    //action();
+    //print(dist_C_AVE + String(", ") + dist_L_AVE + ", " + dist_R_AVE);
 
     // dist_C_AVE = get_dist('C', dist_C, dist_C_AVE);
-    // BT.print("C: ");
-    // BT.println(dist_C_AVE);
-    // dist_L_AVE = get_dist('L', dist_L, dist_L_AVE);
-    // BT.print("L: ");
-    // BT.println(dist_L_AVE);
+    // print(String("C: ") + dist_C_AVE);
 
     current_time = millis() - previous_time;
-    BT.print(x);
-    BT.print(" - t: ");
-    BT.println(current_time);
-    BT.println("---------");
+    print(String("t: ") + current_time);
+    print("---------");
     x++;
     delay(500);
 }
 
 int get_dist(char c, int distArr[], int dist_AVE) {
     count = 0, AVE = 0;
-    // 右: 有 (dist < 4) -> turnLeft 15
     for (i = 0; i < dist_arr_length; i++) {
         distArr[i] = Ultrasound(c);
         // 濾波
@@ -94,72 +89,65 @@ int get_dist(char c, int distArr[], int dist_AVE) {
 // 濾波
 int filter(int distArr[], int dist_AVE, int i) {
     // 第一次平均 0~4
-    if(i <= 4) {
+    if (i <= 4) {
         if (distArr[i] < dist_C_max) {
-            AVE += distArr[i];
             count++;
+            AVE += distArr[i];
         }
         if (i == 4)
-            dist_AVE = AVE / count;
+            return AVE / count;
     } else {
         // 第二次 0~5, 第三次 0~6...
-        if (!(distArr[i] > dist_AVE + 3 || distArr[i] < dist_AVE - 3)) {
-            AVE += distArr[i];
+        if (!(distArr[i] > dist_AVE + 3) || !(distArr[i] < dist_AVE - 3)) {
             count++;
-            dist_AVE = AVE / count;
+            AVE += distArr[i];
+            return AVE / count;
         }
     }
-    //BT.println(dist_AVE);
     return dist_AVE;
 }
 
 void action() {
     if (dist_R_AVE > dist_CLR_min) {
-        turnRight(rotation_15);
-        coast(coast_time);
+        // 因為硬體本身在直走時就會自己稍微右偏前進，所以這裡註解右轉動作
+        // turnRight(rotation_15);
+        // coast(coast_time);
         forward(500);
-        BT.println("Right 15 + For");
+        print("Right 15 + For");
     } else {
         // 如果右邊超音波離牆壁距離低於 5 cm 就向左轉校正
         turnLeft(rotation_15);
         coast(coast_time);
-        BT.println("Left 15");
+        print("Left 15");
 
         dist_C_AVE = get_dist('C', dist_C, dist_C_AVE);
-        BT.print("C: ");
-        BT.println(dist_C_AVE);
+        print(String("C: ") + dist_C_AVE);
         
         if (dist_C_AVE > dist_CLR_min) {
             forward(500);
-            BT.println("For");
+            print("For");
         } else {
             backward(200);
-            BT.println("Back");
-            dist_L_AVE = get_dist('Left', dist_L, dist_L_AVE);
-            BT.print("L: ");
-            BT.println(dist_L_AVE);
+            print("Back");
             
             if (dist_L_AVE > dist_CLR_min) {
                 turnLeft(rotation_90);
                 coast(coast_time);
-                BT.println("Left 90");
+                print("Left 90");
                 //forward(1000);
             } else {
                 turnLeft(rotation_180);
                 coast(coast_time);
-                BT.println("Turn 180");
+                print("Turn 180");
                 //forward(1000);
             }
         }
     }
 }
 
-void print_dist() {
-    BT.print(dist_C_AVE);
-    BT.print(", ");
-    BT.print(dist_L_AVE);
-    BT.print(", ");
-    BT.println(dist_R_AVE);
+void print(String s) {
+    // print(s);
+    Serial.println(s);
 }
 
 // 設定L298n控制腳位為輸出
@@ -174,42 +162,42 @@ void forward(int time) {
     motorAForward();
     motorBForward();
     delay(time);
-    //BT.println("Forward");
+    //print("Forward");
 }
  
 void backward(int time) {
     motorABackward();
     motorBBackward();
     delay(time);
-    //BT.println("Backward");
+    //print("Backward");
 }
  
 void turnRight(int time) {
     motorABackward();
     motorBForward();
     delay(time);
-    //BT.println("Right");
+    //print("Right");
 }
  
 void turnLeft(int time) {
     motorAForward();
     motorBBackward();
     delay(time);
-    //BT.println("Left");
+    //print("Left");
 }
  
 void coast(int time) {
     motorACoast();
     motorBCoast();
     delay(time);
-    //BT.println("Stop");
+    //print("Stop");
 }
  
 void brake(int time) {
     motorABrake();
     motorBBrake();
     delay(time);
-    //BT.println("brake");
+    //print("brake");
 }
 // ------------------------------------------------
 //motor A controls
@@ -266,19 +254,15 @@ long Ultrasound(char c) {
     // 所以 echo, (high 的時間 uS / 2) / 29.1 => 
     // high 的時間 uS/(2 * 29.1) => high 的時間 uS / 58.2
     int EchoPin, TrigPin;
-    switch(c) {
-        case 'C':  
-            EchoPin = Echo_Pin_C;
-            TrigPin = Trig_Pin_C;
-            break;  
-        case 'L':  
-            EchoPin = Echo_Pin_L;
-            TrigPin = Trig_Pin_L;
-            break;  
-        case 'R':  
-            EchoPin = Echo_Pin_R;
-            TrigPin = Trig_Pin_R;
-            break;  
+    if (c == 'L') {
+        EchoPin = Echo_Pin_L;
+        TrigPin = Trig_Pin_L;
+    } else if (c == 'R') {
+        EchoPin = Echo_Pin_R;
+        TrigPin = Trig_Pin_R;
+    } else {
+        EchoPin = Echo_Pin_C;
+        TrigPin = Trig_Pin_C;
     }
     unsigned int dist = ping(EchoPin, TrigPin) / 58;  //calculate distance
     return dist;
@@ -286,8 +270,8 @@ long Ultrasound(char c) {
 
 // 超音波 high 的時間
 unsigned long ping(int EchoPin, int TrigPin) { 
-    //digitalWrite(TrigPin, LOW);
-    //delayMicroseconds(2);
+    digitalWrite(TrigPin, LOW);
+    delayMicroseconds(2);
     // send 10us pulse to HC-SR04 trigger pin
     digitalWrite(TrigPin, HIGH);
     // 超音波只要超過 10us 就好
@@ -296,6 +280,8 @@ unsigned long ping(int EchoPin, int TrigPin) {
     // 傳回 per pulse 時間
     return pulseIn(EchoPin, HIGH);
 }
+
+
 
 
 
